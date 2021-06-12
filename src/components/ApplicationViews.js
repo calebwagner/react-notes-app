978349ce5eb6cade13a097382b643d967632df02
Caleb Wagner <calebwagner5@gmail.com>
2021-06-12 15:46:10 -0500

    add notebook feature

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,12 +4,16 @@ import { NotebookProvider } from "./notebooks/NotebookProvider";
 import { NotebookList } from "./notebooks/NotebookList";
 import { NotesProvider } from "./notes/NotesProvider";
 import { NotesList } from "./notes/NotesList";
+import { NotebookForm } from "./notebooks/NotebookForm";
 
 export const ApplicationViews = () => {
   return (
     <>
       <NotebookProvider>
         <NotesProvider>
+          <Route path="/">
+            <NotebookForm />
+          </Route>
           <Route exact path="/">
             <NotebookList />
           </Route>
---
+++ src/components/notebooks/NotebookForm.js
@@ -0,0 +1,74 @@
+import React, { useContext, useEffect, useState } from "react";
+import { NotebookContext } from "./NotebookProvider";
+import { useHistory } from "react-router-dom";
+
+export const NotebookForm = () => {
+  const { addNotebook } = useContext(NotebookContext);
+  const { notebooks, getNotebooks } = useContext(NotebookContext);
+
+  const currentUserId = parseInt(localStorage.getItem("wwi__user"));
+
+  const [notebook, setNotebook] = useState({
+    title: "",
+    userId: currentUserId,
+    timestamp: Date.now(),
+  });
+
+  const history = useHistory();
+
+  /*
+  Reach out to the world and get customers state
+  and locations state on initialization.
+  */
+  useEffect(() => {
+    getNotebooks().then();
+  }, []);
+
+  const handleControlledInputChange = (event) => {
+    const newNotebook = { ...notebook };
+    newNotebook[event.target.id] = event.target.value;
+    setNotebook(newNotebook);
+  };
+
+  const handleClickSaveNotebook = (event) => {
+    event.preventDefault(); //Prevents the browser from submitting the form
+
+    const notebookTitle = parseInt(notebook.title);
+
+    if (notebookTitle === 0) {
+      window.alert("Please select a type a title");
+    } else {
+      const newNotebook = {
+        title: notebook.title,
+        userId: currentUserId,
+        timestamp: Date.now(),
+      };
+      addNotebook(newNotebook).then(() => history.push("/"));
+    }
+  };
+
+  return (
+    <form className="notebookForm">
+      <h2 className="notebookForm__title">New Notebook</h2>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="title">Notebook Title:</label>
+          <input
+            type="text"
+            id="title"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="type title here ..."
+            value={notebook.title}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+
+      <button className="btn btn-primary" onClick={handleClickSaveNotebook}>
+        Add Notebook
+      </button>
+    </form>
+  );
+};
