0d8aa6cbb3d59299d465a9d21e3ea08ced6d7449
Caleb Wagner <calebwagner5@gmail.com>
2021-06-16 10:00:21 -0500

    edited NotesList to include search feature

--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -1,44 +1,79 @@
 import React, { useContext, useEffect, useState } from "react";
 import { NotesContext } from "./NotesProvider";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import "./Notes.css";
 import { NotebookContext } from "../notebooks/NotebookProvider";
 
+// export const NotesList = () => {
+//   const { notes, getNotes, searchTerms } = useContext(NotesContext);
+//   const [note, setNote] = useState({ notebook: {} });
+//   const { notebooks, getNotebooks } = useContext(NotebookContext);
+//   const { notebookId } = useParams();
+//   const [filteredNotes, setFilteredNotes] = useState([]);
+
+//   useEffect(() => {
+//     getNotes();
+//   }, []);
+
+//   useEffect(() => {
+//     getNotebooks();
+//   }, []);
+
+//   // TODO:
+//   useEffect(() => {
+//     const filtered = notes.filter((note) => {
+//       return parseInt(notebookId) === note.notebookId;
+//     });
+//     setFilteredNotes(filtered);
+//   }, [notes]);
+
+//   return (
+//     <>
+//       <section className="note__list">
+//         {filteredNotes.map((note) => (
+//           <div key={note.id} className="notes" id={`note--${note.id}`}>
+//             <Link className="note" key={note.id}>
+//               {note.title}
+//             </Link>
+//             {note.title}
+//           </div>
+//         ))}
+//       </section>
+//     </>
+//   );
+// };
+
 export const NotesList = () => {
-  const { notes, getNotes } = useContext(NotesContext);
-  const [note, setNote] = useState({ notebook: {} });
-  const { notebooks, getNotebooks } = useContext(NotebookContext);
-  const { notebookId } = useParams();
-  const [filteredNotes, setFilteredNotes] = useState([]);
+  const { notes, getNotes, searchTerms } = useContext(NotesContext);
+  const [filteredNotes, setFiltered] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     getNotes();
   }, []);
 
   useEffect(() => {
-    getNotebooks();
-  }, []);
-
-  // TODO:
-  useEffect(() => {
-    const filtered = notes.filter((note) => {
-      return parseInt(notebookId) === note.notebookId;
-    });
-    setFilteredNotes(filtered);
-  }, [notes]);
+    if (searchTerms !== "") {
+      const notesFiltered = notes.filter((note) =>
+        note.title.toLowerCase().includes(searchTerms.toLowerCase())
+      );
+      setFiltered(notesFiltered);
+    } else {
+      setFiltered(notes);
+    }
+  }, [searchTerms, notes]);
 
   return (
     <>
-      <section className="note__list">
-        {filteredNotes.map((note) => (
-          <div key={note.id} className="notes" id={`note--${note.id}`}>
-            <Link className="note" key={note.id}>
-              {note.title}
-            </Link>
-            {note.title}
-          </div>
-        ))}
-      </section>
+      <h1>Notes:</h1>
+      {/* <button onClick={() => history.push(`/detail/:notebookId/create`)}>
+        Create Note
+      </button> */}
+      <div>
+        {filteredNotes.map((note) => {
+          return <NoteDetail key={note.id} note={note} />;
+        })}
+      </div>
     </>
   );
 };
