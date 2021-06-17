1dc07ee0afee362d4925b3b03196cc51f94b5b61
Caleb Wagner <calebwagner5@gmail.com>
2021-06-17 13:21:47 -0500

    added footer to home page

---
+++ src/components/nav/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+export const Footer = (props) => {
+  return (
+    <ul className="footer">
+      <li className="footer__item">
+        <Link className="footer__link" to="/">
+          About
+        </Link>
+      </li>
+      <li className="footer__item">
+        <Link className="footer__link" to="/">
+          Contact
+        </Link>
+      </li>
+      <li className="footer__item">
+        <Link className="footer__link" to="/">
+          Copyright © 2021
+        </Link>
+      </li>
+    </ul>
+  );
+};
